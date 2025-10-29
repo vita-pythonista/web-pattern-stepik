e68e5aafd9d2cf8ce60367225286238352041ded
Вита Адучиев <joyniter@MacBook-Pro-Vita.local>
2025-10-30 01:15:40 +0300

    test(fix): update using object pages according by base test

--- tests/test_login_and_post.py
+++ tests/test_login_and_post.py
@@ -8,13 +8,12 @@ class TestPost(BaseTest):
     @allure.story("Check login as admin and make post")
     def test_publish_post(self):
         post_text = "Случайный текст"
-        self.login_page.open()
-        self.login_page.login_as_user(username=self.data.LOGIN,
+        self.login_page().open()
+        self.login_page().login_as_user(username=self.data.LOGIN,
                                       password=self.data.PASSWORD)
-        self.home_page.wait_load_page()
-        self.home_page.create_post(post_text)
-        posts = self.home_page.find_published_posts()
+        self.home_page().wait_load_page()
+        self.home_page().create_post(post_text)
+        posts = self.home_page().find_published_posts()
         posts_with_text = [post for post in posts
                            if post_text in post.text]
         assert posts_with_text, f"Any posts with text {post_text} are not published"
-
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,3 +1,5 @@
+import time
+
 import allure
 import pytest
 from base.base_test import BaseTest
@@ -9,20 +11,20 @@ class TestRegistationPage(BaseTest):
 
     @allure.story("Check positive registration form")
     def test_registration(self, fake_user):
-        self.registation_page.open()
-        self.registation_page.input_firstname(firstname=fake_user['firstname'])
-        self.registation_page.input_lastname(lastname=fake_user['lastname'])
-        self.registation_page.input_email(email=fake_user['email'])
-        self.registation_page.input_email_re(email=fake_user['email'])
-        self.registation_page.input_username(username=fake_user['username'])
-        self.registation_page.input_password(password=fake_user['password'])
-        self.registation_page.click_birthdate()
-        self.registation_page.select_current_birthdate()
-        self.registation_page.select_sex()
-        self.registation_page.check_gdpr_agree()
-        self.registation_page.create_screenshot_before_send_request()
-        self.registation_page.click_create_account()
-        self.registation_page.assert_positve_message_about_creation()
+        self.registation_page().open()
+        self.registation_page().input_firstname(firstname=fake_user['firstname'])
+        self.registation_page().input_lastname(lastname=fake_user['lastname'])
+        self.registation_page().input_email(email=fake_user['email'])
+        self.registation_page().input_email_re(email=fake_user['email'])
+        self.registation_page().input_username(username=fake_user['username'])
+        self.registation_page().input_password(password=fake_user['password'])
+        self.registation_page().click_birthdate()
+        self.registation_page().select_current_birthdate()
+        self.registation_page().select_sex()
+        self.registation_page().check_gdpr_agree()
+        self.registation_page().create_screenshot_before_send_request()
+        self.registation_page().click_create_account()
+        self.registation_page().assert_positve_message_about_creation()
 
     @allure.story("Check positive registration form with 3 users")
     @pytest.mark.parametrize("add_users", [2], indirect=True)
@@ -31,10 +33,12 @@ class TestRegistationPage(BaseTest):
         self.registation_page().open()
         self.registation_page().input_firstname(firstname=fake_user['firstname'])
         self.registation_page().input_lastname(lastname=fake_user['lastname'])
+        time.sleep(2)
         # действия 2-го пользователя
         self.registation_page(user2).open()
         self.registation_page(user2).input_firstname(firstname=fake_user['firstname'])
         self.registation_page(user2).input_lastname(lastname=fake_user['lastname'])
+        time.sleep(2)
         # действия 3-го пользователя
         self.registation_page(user3).open()
         self.registation_page(user3).input_firstname(firstname=fake_user['firstname'])
